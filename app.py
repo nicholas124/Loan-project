5cc4a595f8ca0ea5b824bbe75d98a2d6cb30fb55
Nicholas Muchelemba <51362195+nicholas124@users.noreply.github.com>
2022-11-19 13:57:55 +0200

    1

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from explore_page import show_explore_page
 #set up the stream lit application
 st.set_page_config(layout="wide")
 st.sidebar.image("Home_loan.jpeg", use_column_width=True)
-page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore"))
+page = st.sidebar.selectbox("Explore or Classify", ("Classify", "Explore"))
 
 
-if page == "Predict":
+if page == "Classify":
     show_predict_page()
 else:
     show_explore_page()
